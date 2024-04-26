05fdc40a529c93010123c69d3de212435223e41b
Jemutai <janetjemutai19@gmail.com>
2024-04-26 12:47:49 +0300

    initial commit,,added dependencies

---
+++ app/src/main/java/com/jemutai/roomdb/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.jemutai.roomdb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jemutai.roomdb.ui.theme.RoomDBTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoomDBTheme {
+
+            }
+        }
+    }
+}
+
